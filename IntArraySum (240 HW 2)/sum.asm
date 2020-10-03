81e9ba46807c9c3b09061b3afbbb470f185ccc93
Josh Come Ibad <joshcibad@gmail.com>
2020-10-03 11:35:52 -0700

    Update sum.asm

--- IntArraySum (240 HW 2)/sum.asm
+++ IntArraySum (240 HW 2)/sum.asm
@@ -26,7 +26,7 @@
 ; Program name: "Sum of Array of Integers"
 ; Programming languages: One module in C, three modules in x86, one module in C++
 ; Date program began:	 2020-Sep-13
-; Date program completed: 2020-Sep-xx
+; Date program completed: 2020-Oct-03
 ; Files in program:	main.c, manager.asm, input_array.asm, sum.asm, display_array.cpp
 ; Status: Work in Progress
 ;
@@ -48,33 +48,25 @@
 extern printf
 extern scanf
 
-global circle
+global sum
 
-segment .data
 
 ; -----
 ; Initialize data to be used
-
-welcomemsg db "This circle function is brought to you by Josh Ibad", 10, 0
-promptmsg db "Please enter the radius of a circle in whole number of meters: ", 0
+segment .data
 stringoutputformat db "%s", 0
-inputverifyformat db "The number %1d was received.", 10, 0
-signedintegerinputformat db "%ld", 0
-outputmsg db "The circumference of a circle with this radius is %ld and %d/7 meters.", 10, 0
-exitmsg db "The integer part of the area will be returned to the main program. Please enjoy your circles.", 10, 0
-errormsg db "Results are too large for a 64 bit integer.", 10, 0
-
-segment .bss
+errormsg db "Summation caused overflow over signed 64-bit integer. Hence, summation has been terminated", 10, 0
 
 ; -----
 ; Empty segment
+segment .bss
 
 segment .text
-circle:
+sum:
 
 ; -----
 ; Routine Prologue
-;Back up registers to stack to preserve register data
+; Back up registers to stack to preserve register data
 push rbp
 mov  rbp,rsp
 push rdi
@@ -94,98 +86,37 @@ pushf
 push qword -1	;Push extra to even out offset to 16
 
 ; -----
-; Output welcome message and prompt for inputs
-;Output welcome message
-mov qword rdi, stringoutputformat
-mov qword rsi, welcomemsg
-mov qword rax, 0
-call printf
+; Sum loop
+; rdi has been preserved, containing the address of the array
+; rsi has been preserved, containig the length of the array
+xor rcx, rcx		; Zero out rcx, the loop counter
+xor rax, rax		; Zero out rax, the sum accumulator variable 
 
-;Prompt for radius
-mov qword rdi, stringoutputformat	;Consider removing<RRR>
-mov qword rsi, promptmsg
-mov qword rax, 0					;Consider remove<RRR>
-call printf
+sumloop:
+cmp rcx, rdi
+jge sumloopexit
+add rax, qword [rdi]		
+jo overflowerror	; If overflow error met, jump out of loop and run
+					; error handling code.
+add rdi, 8			; Increment the address by a long int's size (8 bytes)
+inc rcx				; Increment the counter register
+jmp sumloop
 
-;Input first integer
-mov qword rdi, signedintegerinputformat
-push qword -1						;WHY, Consider altering <RRR>
-mov qword rsi, rsp					;WHY, Consider altering <RRR>
-mov qword rax, 0
-call scanf
-pop qword r15	;Inputted radius integer
-
-;Output the input
-mov qword rdi, inputverifyformat
-mov rsi, r15
-mov qword rdx, r15					;WHY, Both rsi and rdx hold inputted value
-mov qword rax, 0
-call printf
-
-; -----
-; Calculations ----- Have yet to do;
-; Due to input being a distance, inputs and outputs are assumed to be positive
-; Circumfernce: C = 2r*22/7
-mov qword rax, 44
-mov qword rdx, 0
-mul r15
-
-cmp rdx, 0
-jne overflowerror
-
-mov qword r14, 7
-div r14
-
-;Output circumference
-mov qword rdi, inputverifyformat
-mov rsi, rax
-	;rdx already contains remainder
-mov qword rax, 0
-call printf
-
-
-; Area: A = r*r*22 / 7
-mov qword rax, r15
-mov qword rdx, 0
-mul r15
-
-cmp rdx, 0
-jne overflowerror
-
-mov qword r14, 22
-mul r14
-
-cmp rdx, 0
-jne overflowerror
-
-mov qword r14, 7
-div r14
-
-push rax	;Store area in stack
-jmp finale
-
-overflowerror:
+overflowerror:		
 mov qword rdi, stringoutputformat
 mov qword rsi, errormsg
 mov qword rax, 0
 call printf
-mov rax, -1
-push rax
+; The rax register already has eroneous result and has been noted in the
+; printed message, hence the code does not bother to alter the register
+; to another eroneous value (all values in the domain are valid values, 
+; no return value is available to be exclusively used for overflow errors)
 
-; -----
-; Output final results and exit messages
-;Output exit message
-finale:
-mov qword rdi, stringoutputformat
-mov qword rsi, exitmsg
-mov qword rax, 0
-call printf
+sumloopexit:		; Loop exit. Empty since no further processing required.
 
 ; -----
 ; Routine Epilogue
-;Restore registers to original state
-pop rax		;Retrieve obtained area
-
+; Restore registers to original state
 pop r8		;Remove extra -1 used to oven out offset, register is arbitrary
 popf
 pop rbx
